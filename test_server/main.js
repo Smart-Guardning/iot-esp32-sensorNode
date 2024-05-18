ab67224227d44f93de7005e02763ead17d1eba5a
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-05-18 21:49:04 +0900

    FIX - tset_server 노드 선택 기능

--- test_server/main.js
+++ test_server/main.js
@@ -12,21 +12,32 @@ const webPort = 3000;
 const mqttBrokerUrl = 'mqtt://192.168.1.190:1883'; // MQTT 브로커 주소
 const mqttClient = mqtt.connect(mqttBrokerUrl);
 
+// 현재 구독 중인 노드 ID 저장
+let nodes = new Set();
+
 // MQTT 클라이언트 설정
 mqttClient.on('connect', () => {
     console.log('Connected to MQTT broker');
-    mqttClient.subscribe('smartfarm/#', (err) => {
+    mqttClient.subscribe('smartfarm/sensor/#', (err) => {
         if (err) {
             console.error('Failed to subscribe to topic', err);
         } else {
-            console.log('Subscribed to topic: smartfarm/#');
+            console.log('Subscribed to topic: smartfarm/sensor/#');
         }
     });
 });
 
 mqttClient.on('message', (topic, message) => {
     console.log(`Message received on topic ${topic}: ${message.toString()}`);
-    io.emit('mqttMessage', { topic, message: message.toString() });
+    const payload = JSON.parse(message.toString());
+    const nodeID = payload.node_id;
+
+    if (!nodes.has(nodeID)) {
+        nodes.add(nodeID);
+        io.emit('updateNodes', Array.from(nodes));
+    }
+
+    io.emit('mqttMessage', { nodeID, topic, message: message.toString() });
 });
 
 mqttClient.on('error', (err) => {
@@ -46,6 +57,9 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('Socket.io client disconnected:', socket.id);
     });
+
+    // 클라이언트 연결 시 현재 노드 목록 전송
+    socket.emit('updateNodes', Array.from(nodes));
 });
 
 server.listen(webPort, () => {
