da1c1bd7d5d74875d1a0f1fa5807652937274f9e
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-05-18 21:40:48 +0900

    FIX - 센서노드와 맞게 노드 ID로 토픽전달 하도록 수정

--- test_dummy_esp_server/main.js
+++ test_dummy_esp_server/main.js
@@ -12,12 +12,12 @@ const mqttClient = mqtt.connect(mqttBrokerUrl);
 
 // 센서 데이터 생성 함수
 function generateSensorData() {
-    const nodeID = 'smart-sensor-ZERO'; // 더미 노드 ID
+    const nodeID = 'smartSensor-ZERO'; // 더미 노드 ID
     const soilMoisture = Math.floor(Math.random() * 1024); // 0-1023 사이의 랜덤 값
     const waterLevel = Math.floor(Math.random() * 1024);   // 0-1023 사이의 랜덤 값
     const temperature = (Math.random() * 30 + 10).toFixed(2); // 10-40도 사이의 랜덤 값
     const humidity = (Math.random() * 50 + 30).toFixed(2);    // 30-80% 사이의 랜덤 값
-    //릴레이 스위치 상태
+    // 릴레이 스위치 상태
     const waterpipe = Math.floor(Math.random() * 2); // 0-1 사이의 랜덤 값
     const errorCode = Math.floor(Math.random() * 2); // 0-1 사이의 랜덤 값
 
@@ -40,12 +40,13 @@ mqttClient.on('connect', () => {
     setInterval(() => {
         const sensorData = generateSensorData();
         const payload = JSON.stringify(sensorData);
+        const topic = `smartfarm/sensor/${sensorData.node_id}`; // 고유한 주제 사용
 
-        mqttClient.publish('smartfarm/sensor', payload, {}, (err) => {
+        mqttClient.publish(topic, payload, {}, (err) => {
             if (err) {
                 console.error('Failed to publish sensor data', err);
             } else {
-                console.log('Sensor data published:', payload);
+                console.log(`Sensor data published to ${topic}: ${payload}`);
             }
         });
     }, 3000);
